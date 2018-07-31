d6a8ac3078822cb044f69904352c5b0fccb2639c
Zara <zara@gene.co.uk>
2018-07-31 20:50:36 +0100

    move sw outside, update paths

---
+++ sw.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'static-cache';
+var urlsToCache = [
+  '.',
+  'index.html',
+  'css/main.css',
+  'js/main.js'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(cacheName)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
+
