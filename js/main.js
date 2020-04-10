e6fc2827ce34aaaf6035b3bf34c60f4b5b15675c
Zara <zara@gene.co.uk>
2020-04-10 14:49:19 +0100

    show list of saved notes

--- js/main.js
+++ js/main.js
@@ -24,27 +24,58 @@ if(!navigator.onLine) {
 //	Check that localstorage is supported
 //
 if (typeof(Storage) !== "undefined") {
-    var form = document.querySelector('.js-save'),
-    note = document.querySelector('.js-note');
-    
-    // Store The Note 
-    function storeNote(event){
+    var form = document.querySelector('.js-save');
+
+    form.addEventListener('submit', updateNotes);
+
+    showNotes();
+
+} else {
+	alert('Sorry, I am unable to save this');
+}
+
+function updateNotes(event) {
+        var note = document.querySelector('.js-note'), 
+            savedNotes = [];
 
         // Don't submit
         event.preventDefault();
 
-    	savedNote = note.value;
-    	localStorage.setItem('saved', savedNote);
-    };
+        savedNotes.push(note.value);
+        note.value = '';
 
-    form.addEventListener('submit', storeNote);
+        localStorage.setItem('notes', JSON.stringify(savedNotes));
 
-    // Check if there is a stored note already
-    if (localStorage.getItem('saved')){
-    	// Add it in to our textarea
-		note.innerHTML = localStorage.getItem('saved');
+        showNotes();
+};
+
+function showNotes() {
+    var storedNotes = localStorage.getItem("notes");
+
+    if (storedNotes) {
+
+        var notesArray = JSON.parse(storedNotes),
+            container = document.querySelector('.js-saved-notes'),
+            emptyMessage = document.querySelector('.js-saved-notes--empty'),
+            notesCollection = document.createDocumentFragment(),
+            notesContainer = document.createElement('ul');
+
+        emptyMessage.style.display = "none"; 
+
+        notesContainer.classList.add('take-note__saved-list');
+
+        // Create Li
+        for (var index = 0; index < notesArray.length; index++) {
+
+            var listItem = document.createElement('li');
+                listItem.classList.add('take-note__saved-list-item')
+                listItem.innerHTML = notesArray[index];
+                
+            // append
+            notesCollection.appendChild(listItem);
+        }
+
+        notesContainer.appendChild(notesCollection);
+        container.appendChild(notesContainer);
     } 
-
-} else {
-	alert('Sorry, I am unable to save this');
 }
