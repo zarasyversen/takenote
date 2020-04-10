1bad5758a1ef451a98f4dc28210d78cc205e366c
Zara <zara@gene.co.uk>
2020-04-10 12:45:29 +0100

    move sw registration to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+
+//
+// Register Service Worker
+//
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then(function(registration) {
+        console.log('Registered:', registration);
+    })
+    .catch(function(error) {
+        console.log('Registration failed: ', error);
+    });
+}
+
 //
 // Change message if user is offline 
 //
