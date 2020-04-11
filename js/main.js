aaf5a14c3bde09826c58dcd41787680191bed14f
Zara <zara@gene.co.uk>
2020-04-11 17:22:35 +0100

    rewrite js, better naming and now list is updating on submit and on refresh

--- js/main.js
+++ js/main.js
@@ -26,56 +26,91 @@ if(!navigator.onLine) {
 if (typeof(Storage) !== "undefined") {
     var form = document.querySelector('.js-save');
 
-    form.addEventListener('submit', updateNotes);
+    form.addEventListener('submit', addNoteToList);
 
-    showNotes();
-
+    createList(); 
 } else {
-	alert('Sorry, I am unable to save this');
+    alert('local storage not supported');
 }
 
-function updateNotes(event) {
-        var note = document.querySelector('.js-note'), 
-            savedNotes = [];
+//
+// Add Note To Local Storage
+//
+function addNoteToList(event) {
+        var note = document.querySelector('.js-note'),
+            storedNotes = localStorage.getItem("notes")
+            savedNotes = storedNotes ? JSON.parse(storedNotes) : [];
 
-        // Don't submit
+        // Ignore submit form
         event.preventDefault();
 
+        // Add our note to the array and empty the textarea
         savedNotes.push(note.value);
         note.value = '';
 
+        // Add the array to local storage
         localStorage.setItem('notes', JSON.stringify(savedNotes));
 
-        showNotes();
+        updateList();
 };
 
-function showNotes() {
+//
+// Update List
+//
+function updateList() {
+    var notesList = document.querySelector('.take-note__saved-list'),
+        storedNotes = JSON.parse(localStorage.getItem("notes"));
+
+    // Check if list already exists
+    if (notesList) {
+
+        // Create <li> with the latest item from the array
+        var latestNote = storedNotes[storedNotes.length - 1],
+            listItem = document.createElement('li');
+
+        listItem.classList.add('take-note__saved-list-item');
+        listItem.innerHTML = latestNote;
+
+        // Add in to our list
+        notesList.appendChild(listItem)
+    } else {
+        createList();
+    }
+
+}
+
+//
+// Create List - Creates <ul> with <li>
+//
+function createList() {
     var storedNotes = localStorage.getItem("notes");
 
     if (storedNotes) {
 
         var notesArray = JSON.parse(storedNotes),
             container = document.querySelector('.js-saved-notes'),
-            emptyMessage = document.querySelector('.js-saved-notes--empty'),
-            notesCollection = document.createDocumentFragment(),
-            notesContainer = document.createElement('ul');
+            emptyMessage = document.querySelector('.js-saved-notes--empty');
+
+        // Create Elements
+        var notesCollection = document.createDocumentFragment(),
+            notesList = document.createElement('ul');
+
+        notesList.classList.add('take-note__saved-list');
 
         emptyMessage.style.display = "none"; 
 
-        notesContainer.classList.add('take-note__saved-list');
-
-        // Create Li
+        // Create Each List Item
         for (var index = 0; index < notesArray.length; index++) {
 
             var listItem = document.createElement('li');
                 listItem.classList.add('take-note__saved-list-item')
                 listItem.innerHTML = notesArray[index];
-                
-            // append
+            
             notesCollection.appendChild(listItem);
         }
 
-        notesContainer.appendChild(notesCollection);
-        container.appendChild(notesContainer);
+        // Add notes in to list & add list in to container
+        notesList.appendChild(notesCollection);
+        container.appendChild(notesList);
     } 
 }
